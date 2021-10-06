3d8b1bcc38cc051cc0a75b66c3b0f13296049f39
yaguchi109 <yaguchi109@gmail.com>
2021-10-07 04:05:19 +0900

    auto commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+const timeZoneHour =
+  1000 * 60 * 60 * Number(process.env.NEXT_PUBLIC_TIMEZONE_HOUR);
+const timeZoneMinute =
+  1000 * 60 * Number(process.env.NEXT_PUBLIC_TIMEZONE_MINUTE);
+const getDate = () => {
+  const now = new Date();
+  now.setTime(now.getTime() + timeZoneHour + timeZoneMinute);
+  return now.toISOString().substr(0, 10);
+};
+
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   distDir: '.next',
+  env: {
+    NEXT_PUBLIC_EXPORT_DATE: getDate(),
+  },
 };
